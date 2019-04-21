43edb63ccf086164710fa7ccffa85f181dbfd549
cd15243 <875865243@qq.com>
2019-04-21 15:13:28 +0800

    ex2.1

---
+++ ex2/源.cpp
@@ -0,0 +1,16 @@
+//第二章  2.1
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "姓名：张三" << endl;
+	cout << "地址：xx省xx市xx区xxx弄xx号xx室" << endl;
+
+	system("pause");
+	//cin.get();
+
+	return 0;
+}
